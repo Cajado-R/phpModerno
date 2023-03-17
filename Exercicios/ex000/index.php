49c61c4468ffedbbd8b218339fe12758ac1ee93f
Cajado-R <claudiocajado@Outlook.com.br>
2023-03-17 15:09:56 -0300

    Exercícios iniciais

---
+++ Exercicios/ex000/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício Inicial (OLÁ MUNDO)</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "OLÁ MUNDO! \u{1F30E}";
+        ?>
+    </h1>
+    <p>
+        Livres!!
+    </p>
+</body>
+</html>
