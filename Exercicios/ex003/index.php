c47b86aec769e03c23e1e510ad1d868fbd79e14b
Cajado-R <claudiocajado@Outlook.com.br>
2023-03-28 17:30:50 -0300

    Create index.php

---
+++ Exercicios/ex003/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos em PHP</title>
+</head>
+<body>
+    <h1>Teste de Tipos Primitivos</h1>
+    <?php 
+    //Um número iniciado em 0 utiliza bases numéricas.
+    $num = 010;
+    echo "O valor da variável é $num";
+    
+    $v = 45.2;
+    //Para receber todas as informações da variavel usa-se var dump
+    var_dump($v);
+    ?>
+</body>
+</html>
